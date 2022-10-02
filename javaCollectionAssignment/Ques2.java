6eebf007e4bc052825542088c8a260742fab5c19
Nafees27 <114075486+Nafees27@users.noreply.github.com>
2022-10-02 22:05:07 +0530

    Update Ques2.java

--- javaCollectionAssignment/Ques2.java
+++ javaCollectionAssignment/Ques2.java
@@ -8,18 +8,71 @@
 
 package javaCollectionAssignment;
 
+import java.util.*;
+
 public class Ques2 {
 
 	public static void main(String[] args) 
 	{
-		
+		Employee emp1 = new Employee(27.0,30000.5,"Nafees","Ahmad");
+        	Employee emp2 = new Employee(28.5,40000.6,"Ayaaz","Khan");
+        	Employee emp3 = new Employee(30.1,50000.7,"Saif","Ali");
+
+        	List<Employee> employeeList = new ArrayList<>();
+        	employeeList.add(emp1);
+        	employeeList.add(emp2);
+        	employeeList.add(emp3);
+
+        	Collections.sort(employeeList);
+        	System.out.println(employeeList);
+
+	        employeeList.sort((t1,t2)-> ((int)(t1.getAge() - t2.getAge())));
+        	System.out.println("\n"+employeeList);
 	}
 
 }
 
-class Employee
+class Employee implements Comparable<Employee>
 {
-	double Age;
-	double Salary;
-	String Name;
+    private Double age;
+    private Double salary;
+    private String firstName;
+    private String lastName;
+
+    public Employee(Double age, Double salary, String firstName, String lastName) {
+        this.age = age;
+        this.salary = salary;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Double getAge() {
+        return age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public int compareTo(Employee employee) {
+        if(this.firstName.equalsIgnoreCase(employee.getFirstName()))
+            return (this.lastName.compareToIgnoreCase(employee.getLastName()));
+
+        return (this.firstName.compareToIgnoreCase(employee.getFirstName()));
+    }
+
+    @Override
+
+    public String toString() {
+        return "FirstName : "+firstName + " LastName : "+ lastName+ " Salary : " + salary + " Age : " + age;
+    }
 }
